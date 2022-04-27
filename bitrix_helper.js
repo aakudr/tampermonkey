2d563fdfed69156cc96208a071d343edb17cca3a
aakudr <aakudr2k3@gmail.com>
2022-04-27 12:59:13 +0300

    Update bitrix_helper.js

--- bitrix_helper.js
+++ bitrix_helper.js
@@ -1080,7 +1080,7 @@ const bitrix_helper = function ()
 
                 $(document).ready(function() {
                     appendStyle(".dropdown {position: relative; display: inline-block; padding: 0px 16px; border: 1px solid #eee}\
-.dropdown-content {display: none; position: absolute; background-color: #f9f9f9; min-width: 160px; box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2); padding: 0px 16px; z-index: -1;}\
+.dropdown-content {display: none; position: absolute; background-color: #f9f9f9; min-width: 160px; box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2); padding: 0px 16px; z-index: 10;}\
 .dropdown:hover .dropdown-content {display: block;}\
 .call-line {display: block; height: 2rem; line-height: 2rem;} .call-line:hover {background-color: #eee}")
                     var id = setInterval(function() {
